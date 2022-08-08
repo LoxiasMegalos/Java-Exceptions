573ab755c2540268496d15ebfaab6ab31d989ece
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-08-08 18:18:41 -0300

    Commit projeto inicial

---
+++ test/ContaTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ContaTest{
+
+	@Test
+    public void testConta() {
+        assertTrue(true);
+    }
+}
